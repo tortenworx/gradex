8c8cb96f733022e43aa794760f11c51af231e24e
Jarelle Emmanuel Payad Andulan <me@torten.xyz>
2024-11-24 12:54:58 +0800

    [aws](codepipeline) fixing fixing fixing fixing x7

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   app.enableShutdownHooks();
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(process.env.PORT ?? 80);
 }
 bootstrap();
